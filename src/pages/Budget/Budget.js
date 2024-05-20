174acdfa6e362c2b01f5cdda05e16677c6be021d
byron00123 <omondibyron25@gmail.com>
2024-05-20 17:16:42 +0300

    :tada:start project

---
+++ src/pages/Budget/Budget.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Budget.module.scss';
+import axios from 'axios'; 
+
+const budget = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default budget
