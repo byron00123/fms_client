99e7ad631cfa43ce2f51409add9a948a775bcd2b
byron00123 <omondibyron25@gmail.com>
2024-05-22 11:42:00 +0300

    add material ui

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Home.module.scss';
+import axios from 'axios'; 
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
